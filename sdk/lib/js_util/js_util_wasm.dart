d4aa11481a889e521096c06a82939ca14bf92337
Aske Simon Christensen <askesc@google.com>
2022-01-24 17:33:23 +0100

    Cleaned out js and html patch files
    Change-Id: I4840ff3d10e7f7abf6e8851279821bbe96e143ad

--- sdk/lib/js_util/js_util_wasm.dart
+++
@@ -1,7 +0,0 @@
-library dart.wasm.js_util;
-
-dynamic callMethod(Object o, String method, List<Object?> args) native;
-dynamic newObject() native;
-bool hasProperty(Object o, Object name) native;
-dynamic getProperty(Object o, Object name) native;
-dynamic setProperty(Object o, Object name, Object? value) native;
